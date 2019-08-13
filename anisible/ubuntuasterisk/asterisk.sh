310ecd7e3f76c39aade7d2c841e8baca573719cd
Rogmer Bulaclac <rogmer.bulaclac@gmail.com>
2019-08-14 03:29:28 +0800

    latest commit 08/14/2019

---
+++ anisible/ubuntuasterisk/asterisk.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+###INSTALL ANSIBLE ON UBUNT
+apt-get update -Y
+apt-get install software-properties-common
+apt-add-repository ppa:ansible/ansible
+apt-get update
+apt-get install ansible
+
+
+
+
+
+
